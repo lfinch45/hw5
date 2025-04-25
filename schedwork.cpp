ad4ef674071292966e908e5642d3fbb2bd80d090
Luke Finch <lfinch@usc.edu>
2025-04-25 22:41:49 +0000

    Finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool createSchedule(
+    const AvailabilityMatrix& avail,
+    size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<int>& shiftCount,
+    size_t day,
+    size_t slot
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,10 +44,65 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
 
+    size_t dayCounter = avail.size();
+    size_t workerCounter = avail[0].size();
 
+    sched.resize(dayCounter);
+    std::vector<int> shiftCount(workerCounter, 0);
 
+    return createSchedule(avail, dailyNeed, maxShifts, sched, shiftCount, 0, 0);
+
+}
+
+bool createSchedule(
+    const AvailabilityMatrix& avail,
+    size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<int>& shiftCount,
+    size_t day,
+    size_t slot
+)
+{
+
+    // Base case: at the end of the matrix 
+    if(day == avail.size()){
+        return true;
+    }
+
+    else{
+        // Need to loop through each worker
+        for(Worker_T worker = 0; worker < avail[day].size(); ++worker){
+            // Need to check for 3 things
+            bool workerAvailable = avail[day][worker] == 1;
+            bool workerHasShiftsRemaining = shiftCount[worker] < maxShifts;
+            bool workerNotAlreadyScheduled = std::find(sched[day].begin(), sched[day].end(), worker) == sched[day].end();
+            
+            // Adding to the schedule & recursing if the 3 conditions are met
+            if(workerAvailable && workerHasShiftsRemaining && workerNotAlreadyScheduled){
+                sched[day].push_back(worker);
+                shiftCount[worker]++;
+
+                // Now we recurse
+                if(slot + 1 < dailyNeed){
+                    if(createSchedule(avail, dailyNeed, maxShifts, sched, shiftCount, day, slot + 1)){
+                        return true;
+                    }
+                }
+                else{
+                    if(createSchedule(avail, dailyNeed, maxShifts, sched, shiftCount, day + 1, 0)){
+                        return true;
+                    }
+                }
+
+                // Backtracking
+                sched[day].pop_back();
+                shiftCount[worker]--;
+            }
+        }
+        return false;
+    }
 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void buildWord(
+    std::string& currentWord,
+    const std::string& input,
+    std::string floatingLetters,
+    const std::set<std::string>& dict,
+    std::set<std::string>& resultSet,
+    int currentIndex);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +27,52 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    std::set<std::string> workingWords;
+    std::string currentWord = in;
+    buildWord(currentWord, in, floating, dict, workingWords, 0);
+    return workingWords;
 
 }
 
 // Define any helper functions here
+void buildWord(
+    std::string& currentWord,
+    const std::string& input,
+    std::string floatingLetters,
+    const std::set<std::string>& dict,
+    std::set<std::string>& resultSet,
+    int currentIndex)
+{
+    // Base Case - we've reached the end of the word
+    if(currentIndex == input.length()){
+        if(dict.find(currentWord) != dict.end() && floatingLetters.empty()){
+            resultSet.insert(currentWord);
+        }
+        return;
+    }
+    
+    // Case where current letter is already fixed
+    if(currentWord[currentIndex] != '-'){
+        buildWord(currentWord, input, floatingLetters, dict, resultSet, currentIndex + 1);
+        return;
+    }
+    // Case where current letter is a "-", meaning we have to search for valid letters
+    else{
+        
+        for(char c = 'a'; c <= 'z'; ++c){
+            size_t position = floatingLetters.find(c);
+
+            // If c is in floatingLetters, we'll remove it from floatingLetters because we will have "used it"
+            std::string floatingCopy = floatingLetters;
+            if(position != std::string::npos){
+                floatingCopy.erase(position, 1);
+            }
+            
+            // Place c in the word and recurse
+            currentWord[currentIndex] = c;
+            buildWord(currentWord, input, floatingCopy, dict, resultSet, currentIndex + 1);
+            currentWord[currentIndex] = '-'; // Backtracking
+        }
+    }
+
+}
